ce2eb444da821b38f86e13e9bfeb785cc45db823
Alyssa <alyssapadayhag@gmail.com>
2026-06-10 22:47:56 -0400

    Add fonts and edit header

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,8 @@
 function Layout() {
     return (
         <>
-          <h1 class="text-3xl font-bold text-center">Alyssa Virdeth L Padayhag</h1>
+          <h1 class="text-5xl font-bold text-center bebas-neue-regular">Alyssa Virdeth L Padayhag</h1>
+          <h2 class="text-3xl font-bold text-center bebas-neue-regular">Full Stack Web Developer</h2>
         </>
     )
 }
